760dd67e2b07b841dab7a483ac9b46fa69be6ed4
dani1019 <nihonkita17@gmail.com>
2022-01-04 10:41:15 +0900

    contents +update

--- main.js
+++ main.js
@@ -9,3 +9,46 @@ document.addEventListener('scroll', ()=>{
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//Handle scrolling when tapping on the navbar menuument.querySelector('.navbar__menu');
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    const target= event.target;
+    const link = target.dataset.link;
+    if(link==null){
+        return;
+    }
+    scrollIntoView(link);
+});
+
+//handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click',()=>{
+    scrollIntoView('#contact');
+});
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow-up")
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+arrowUp.addEventListener('click', ()=>{
+    scrollIntoView('#home');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : "smooth"});
+}
