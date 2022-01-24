c795215955b78fab45544f9c2ab6549f34b3e390
dani1019 <nihonkita17@gmail.com>
2022-01-24 20:55:29 +0900

    mywork button

--- main.js
+++ main.js
@@ -52,3 +52,39 @@ function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior : "smooth"});
 }
+
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) =>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter==null){
+        return;
+    }
+
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = 
+        e.target.nodeName === 'BUTTON' ? e.target: e.target.parentNode;
+    e.target.classList.add('selected');
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) =>{
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
+    // for(let project of projects){
+
+    // }
+
+    // let project;
+    // for(let i = 0; i<projects.length; i++){
+    //     project = projects[i]};
